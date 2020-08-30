42d1834ad5865eebdf4e8c5079ff49cb9395501e
muellerzr <muellerzr@gmail.com>
2020-08-30 13:35:36 -0400

    release

--- README.md
+++ README.md
@@ -4,23 +4,10 @@
 
 ## Install
 
-We have 4 modules you can install, depending on your usecase:
-
-* Base Library (Just inference stuff): `pip install fastinference`
-* ONNX Inference: `pip install fastinference[onnx-cpu]` or `fastinference[onnx-gpu]`
-* Interperatability (Class Confusion + SHAP): `pip install fastinference[interp]`
-* Everything: `pip install fastinference[all]`
+`pip install fastinference`
 
 ## Wonderful Contributors:
 
-[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/0)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/0)[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/1)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/1)[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/2)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/2)[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/3)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/3)[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/4)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/4)[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/5)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/5)[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/6)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/6)[![](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/images/7)](https://sourcerer.io/fame/muellerzr/muellerzr/fastinference/links/7)
+(Using both their `fastai` handles as well as their GitHub handles if possible):
 
-
-# Directions for Contributing:
-
-1. Fork this repository into your GitHub Account
-2. Ensure that `nbdev` is installed on your system
-3. Make any changes and ensure that you run the following **before** commiting:
-  * `nbdev_build_lib`
-  * `nbdev_clean_nbs`
-4. Open a Pull Request with the library, and choose "From fork" to open one with the main repository.
+* Pavel (Pak)
--- fastinference/inference/inference.py
+++ fastinference/inference/inference.py
@@ -24,7 +24,6 @@ def _fully_decode(dl:TfmdDL, inps, outs):
     outs.insert(len(outs), dec)
     return outs
 
-# Cell
 @typedispatch
 def _fully_decode(dl:TabDataLoader, inps, outs):
     "Attempt to fully decode the `inp"
@@ -34,7 +33,6 @@ def _fully_decode(dl:TabDataLoader, inps, outs):
     outs.insert(len(outs), dec)
     return outs
 
-# Cell
 def _decode_loss(vocab, dec_out, outs):
     i2c, dec2c = {}, []
     try:
@@ -49,55 +47,44 @@ def _decode_loss(vocab, dec_out, outs):
     return outs
 
 # Cell
+@delegates(GatherPredsCallback.__init__)
 @patch
-def get_preds(x:Learner, ds_idx=1, dl=None, raw_outs=False, decoded_loss=True, fully_decoded=False,
-             **kwargs):
-    "Get predictions with possible decoding"
-    inps, outs, dec_out, raw,n_inp,is_multi = [], [], [], [], x.dls.n_inp,False
-    if dl is None: dl = x.dls[ds_idx].new(shuffle=False, drop_last=False)
-    if n_inp > 1:
-        is_multi = True
-        [inps.append([]) for _ in range(n_inp)]
-    x.model.eval()
-    for batch in dl:
-        with torch.no_grad():
-            if fully_decoded:
-                if is_multi:
-                    for i in range(n_inp):
-                        inps[i].append(batch[i].cpu())
-                else:
-                    inps += batch[:n_inp]
-            out = x.model(*batch[:n_inp])
-            raw.append(out)
-            if decoded_loss or fully_decoded:
-                dec_out.append(x.loss_func.decodes(out))
-    raw = torch.cat(raw, dim=0).cpu().numpy()
-    if fully_decoded or decoded_loss:
-        dec_out = torch.cat(dec_out, dim=0)
-    if not raw_outs:
-        try: outs.insert(0, x.loss_func.activation(tensor(raw)).numpy())
-        except: outs.insert(0, dec_out)
-    else:
-        outs.insert(0, raw)
-    if fully_decoded: outs = _fully_decode(x.dls[0], inps, outs)
-    if decoded_loss: outs = _decode_loss(x.dls.vocab, dec_out, outs)
-    return outs
+def get_preds(self:Learner, ds_idx=1, dl=None, with_input=False, with_decoded=False, with_loss=False, raw=False, act=None,
+                inner=False, reorder=True, cbs=None, **kwargs):
+    if dl is None: dl = self.dls[ds_idx].new(shuffled=False, drop_last=False)
+    if reorder and hasattr(dl, 'get_idxs'):
+        idxs = dl.get_idxs()
+        dl = dl.new(get_idxs = _ConstantFunc(idxs))
+    cb = GatherPredsCallback(with_input=with_input, with_loss=with_loss, **kwargs)
+    ctx_mgrs = self.validation_context(cbs=L(cbs)+[cb], inner=inner)
+    if with_loss: ctx_mgrs.append(self.loss_not_reduced())
+    with ContextManagers(ctx_mgrs):
+        self._do_epoch_validate(dl=dl)
+        if act is None: act = getattr(self.loss_func, 'activation', noop)
+        res = cb.all_tensors()
+        pred_i = 1 if with_input else 0
+        if res[pred_i] is not None:
+            res[pred_i] = act(res[pred_i]) if not raw else res[pred_i]
+            if with_decoded:
+                res.insert(pred_i+2, getattr(self.loss_func, 'decodes', noop)(res[pred_i]))
+        if reorder and hasattr(dl, 'get_idxs'): res = nested_reorder(res, tensor(idxs).argsort())
+        if with_decoded:
+            if hasattr(self.dls, 'categorize'): res[pred_i+1] = apply(getattr(self.dls, 'categorize', noop).decode, [*res[pred_i+2]])
+            if hasattr(self.dls, 'multi_categorize'): res[pred_i+1] = apply(getattr(self.dls, 'multi_categorize', noop).decode, [*res[pred_i+2]])
+        return tuple(res)
+    self._end_cleanup()
 
 # Cell
 @patch
 def predict(x:Learner, item, with_input=False, rm_type_tfms=None):
         dl = x.dls.test_dl([item], rm_type_tfms=rm_type_tfms, num_workers=0)
-        res = x.get_preds(dl=dl, fully_decoded=True)
-        if not with_input: res = res[:-1]
+        res = x.get_preds(dl=dl, with_input=with_input, with_decoded=True)
         return res
 
 # Cell
 @patch
 def predict(x:TabularLearner, row, with_input=False, rm_type_tfms=None):
-        tst_to = x.dls.valid_ds.new(pd.DataFrame(row).T)
-        tst_to.process()
-        tst_to.conts = tst_to.conts.astype(np.float32)
-        dl = x.dls.valid.new(tst_to)
-        res = x.get_preds(dl=dl, fully_decoded=True)
-        if not with_input: res = res[:-1]
+        dl = x.dls.test_dl(row.to_frame().T)
+        dl.dataset.conts = dl.dataset.conts.astype(np.float32)
+        res = x.get_preds(dl=dl, with_decoded=True, with_input=with_input)
         return res
